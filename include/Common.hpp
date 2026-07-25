6aa0c3c0b41d16a385b876d120c6fe5f36ce3b31
Vuqar <vuqarahadli17@gmail.com>
2026-07-26 03:05:54 +0400

    expanding class file

--- include/Common.hpp
+++ include/Common.hpp
@@ -4,6 +4,9 @@
 
 constexpr U4 CLASS_MAGIC = 0xCAFEBABEu;
 
+
+
+
 /**
  * The constant pool tags as defined in the JVM specification.
  */
@@ -26,4 +29,32 @@ enum class ConstantTag : U1
     InvokeDynamic       = 18,
     Module              = 19,
     Package             = 20
+};
+
+/**
+ * Class access and property modifiers (§4.1 ClassFile).
+ * Also reused (with different applicable subsets) for fields
+ * and methods (§4.5, §4.6).
+ */
+enum AccessFlag : U2
+{
+    ACC_PUBLIC       = 0x0001,
+    ACC_PRIVATE      = 0x0002, // fields/methods only
+    ACC_PROTECTED    = 0x0004, // fields/methods only
+    ACC_STATIC       = 0x0008, // fields/methods only
+    ACC_FINAL        = 0x0010,
+    ACC_SUPER        = 0x0020, // classes only (treat superclass methods specially)
+    ACC_SYNCHRONIZED = 0x0020, // methods only (same bit as ACC_SUPER)
+    ACC_VOLATILE     = 0x0040, // fields only
+    ACC_BRIDGE       = 0x0040, // methods only
+    ACC_TRANSIENT    = 0x0080, // fields only
+    ACC_VARARGS      = 0x0080, // methods only
+    ACC_NATIVE       = 0x0100, // methods only
+    ACC_INTERFACE    = 0x0200,
+    ACC_ABSTRACT     = 0x0400,
+    ACC_STRICT       = 0x0800, // methods only
+    ACC_SYNTHETIC    = 0x1000,
+    ACC_ANNOTATION   = 0x2000,
+    ACC_ENUM         = 0x4000,
+    ACC_MODULE       = 0x8000  // classes only
 };
--- include/classfile/ClassFile.hpp
+++ include/classfile/ClassFile.hpp
@@ -9,6 +9,31 @@
 #include "BinaryReader.hpp"
 #include "ConstantPool.hpp"
 
+/**
+ * 
+ * From JVM specification §4
+    ClassFile {
+    u4             magic;                                | COVERED
+    u2             minor_version;                        | COVERED
+    u2             major_version;                        | COVERED
+    u2             constant_pool_count;                  | COVERED
+    cp_info        constant_pool[constant_pool_count-1]; | COVERED
+    u2             access_flags;                         | COVERED
+    u2             this_class;                           | COVERED
+    u2             super_class;                          | COVERED
+    u2             interfaces_count;                     | COVERED
+    u2             interfaces[interfaces_count];         | COVERED
+    u2             fields_count;                         | TODO
+    field_info     fields[fields_count];                 | TODO
+    u2             methods_count;                        | TODO
+    method_info    methods[methods_count];               | TODO
+    u2             attributes_count;                     | TODO
+    attribute_info attributes[attributes_count];         | TODO
+    }
+ * 
+ */
+
+
 class ClassFile
 {
 public:
@@ -25,12 +50,19 @@ public:
     }
 
 private:
+    void saveClassMetadata();
+
     std::string filename;
     U4 magic;
     U2 minorVersion;
     U2 majorVersion;
     U2 constantPoolCount;
+    std::vector<std::unique_ptr<CPInfo>> constantPool;
+
+    U2 accessFlags;
+    U2 thisClass;
+    U2 superClass;
+    std::vector<U2> interfaces;
 
     BinaryReader reader;
-    std::vector<std::unique_ptr<CPInfo>> constantPool;
 };
