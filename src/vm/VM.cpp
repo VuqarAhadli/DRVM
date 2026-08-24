23abf538b82d5270db8ec126013eb2ecc063c55b
VuqarAhadli <vuqarahadli17@gmail.com>
2026-08-24 22:39:03 +0200

    dupx2 opcode

--- src/vm/VM.cpp
+++ src/vm/VM.cpp
@@ -893,7 +893,37 @@ Value VM::execute(ClassFile& classFile, const CodeAttribute& code)
 
                 break;
             }
+            case Opcode::DupX2:
+            {
+                Value val1 = frame.pop();
+                Value val2 = frame.pop();
 
+                bool val1IsCategory2 = std::get_if<F8>(&val1) || std::get_if<S8>(&val1);
+                bool val2IsCategory2 = std::get_if<F8>(&val2) || std::get_if<S8>(&val2);
+
+                if (val1IsCategory2)
+                {
+                    throw std::runtime_error("dup_x2: illegal category-2 value as val1");
+                }
+
+                if (val2IsCategory2)
+                {
+                    frame.push(val1);
+                    frame.push(val2);
+                    frame.push(val1);
+                }
+                else
+                {
+                    Value val3 = frame.pop();
+
+                    frame.push(val1);
+                    frame.push(val3);
+                    frame.push(val2);
+                    frame.push(val1);
+                }
+
+                break;
+            }
 
 
             case Opcode::IAdd:
