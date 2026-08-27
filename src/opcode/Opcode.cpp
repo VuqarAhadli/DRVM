c4c933e9650d7fcb87f33235287998fa75ded3fe
VuqarAhadli <vuqarahadli17@gmail.com>
2026-08-27 23:50:31 +0200

    fixed isimplemented function

--- src/opcode/Opcode.cpp
+++ src/opcode/Opcode.cpp
@@ -3,53 +3,12 @@
 
 bool isImplemented(Opcode opcode)
 {
-    switch (opcode)
+    if (static_cast<U1>(opcode) <= 92)
     {
-        case Opcode::Nop:
-        case Opcode::AConstNull:
-        case Opcode::IConstM1:
-        case Opcode::IConst0:
-        case Opcode::IConst1:
-        case Opcode::IConst2:
-        case Opcode::IConst3:
-        case Opcode::IConst4:
-        case Opcode::IConst5:
-        case Opcode::Ldc:
-        case Opcode::LdcW:
-        case Opcode::Ldc2W:
-        case Opcode::BiPush:
-        case Opcode::SiPush:
-        case Opcode::ILoad:
-        case Opcode::ILoad0:
-        case Opcode::ILoad1:
-        case Opcode::ILoad2:
-        case Opcode::ILoad3:
-        case Opcode::IStore:
-        case Opcode::IStore0:
-        case Opcode::IStore1:
-        case Opcode::IStore2:
-        case Opcode::IStore3:
-        case Opcode::IAdd:
-        case Opcode::ISub:
-        case Opcode::NewArray:
-        case Opcode::ANewArray:
-        case Opcode::ArrayLength:
-        case Opcode::Goto:
-        case Opcode::IReturn:
-        case Opcode::Return:
-        case Opcode::GetStatic:
-        case Opcode::PutStatic:
-        case Opcode::GetField:
-        case Opcode::PutField:
-        case Opcode::InvokeVirtual:
-        case Opcode::InvokeSpecial:
-        case Opcode::InvokeStatic:
-        case Opcode::New:
-       return true;
-
-        default:
-       return false;
+        return true;
     }
+    
+    return false;
 }
 
 std::string toString(Opcode opcode)
