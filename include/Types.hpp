8a3291cfa528345be719a3018cb7fcbe12ceedcc
VuqarAhadli <vuqarahadli17@gmail.com>
2026-09-01 21:29:23 +0200

    type clarification

--- include/Types.hpp
+++ include/Types.hpp
@@ -9,7 +9,7 @@ using U4 = std::uint32_t;
 using U8 = std::uint64_t;
 
 using S1 = std::int8_t;
-using S2 = std::int16_t;
+using S2 = std::int16_t;  // Short
 using S4 = std::int32_t;  // Int
 using S8 = std::int64_t;  // Long 
 
