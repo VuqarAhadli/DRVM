fdc930b64a7a05924348db8efff6579e89366edf
VuqarAhadli <vuqarahadli17@gmail.com>
2026-09-04 23:56:35 +0200

    fix line sequence

--- src/vm/VM.cpp
+++ src/vm/VM.cpp
@@ -2302,29 +2302,22 @@ Value VM::execute(ClassFile& classFile, const CodeAttribute& code)
             {
                 return std::get<S4>(frame.pop());
             }
-
             case Opcode::LReturn:
             {
                 return std::get<S8>(frame.pop());
             }
-
             case Opcode::FReturn:
             {
-
-            }
                 return std::get<F4>(frame.pop());
-
+            }
             case Opcode::DReturn:
             {
-
-            }
                 return std::get<F8>(frame.pop());
-
+            }
             case Opcode::AReturn:
             {
                 return frame.pop();   
             }
-
             case Opcode::Return:
             {
                 return Value();
