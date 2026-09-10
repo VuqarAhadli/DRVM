ebfd9aab2b294852dc57df6290629e8cb94b935e
VuqarAhadli <vuqarahadli17@gmail.com>
2026-09-10 23:30:50 +0200

    invokeinterface opcode

--- src/vm/VM.cpp
+++ src/vm/VM.cpp
@@ -2805,6 +2805,86 @@ Value VM::execute(ClassFile& classFile, const CodeAttribute& code)
 
                 break;
             }
+            case Opcode::InvokeInterface:
+            {
+                auto indexByte1 = bytecode[frame.programCounter];
+                frame.programCounter++;
+                auto indexByte2 = bytecode[frame.programCounter];
+                frame.programCounter++;
+
+                U1 count = bytecode[frame.programCounter];
+                frame.programCounter++;
+
+                U1 reserved = bytecode[frame.programCounter];
+                frame.programCounter++;
+
+                (void) count;
+                (void) reserved;
+
+                U2 index = static_cast<U2>((indexByte1 << 8) | indexByte2);
+
+                ConstantInterfaceMethodref* methodref = classFile.getConstant<ConstantInterfaceMethodref>(index);
+                ConstantNameAndType* nameAndType = classFile.getConstant<ConstantNameAndType>(methodref->nameAndTypeIndex);
+                ConstantUtf8* methodNameUTF8 = classFile.getConstant<ConstantUtf8>(nameAndType->nameIndex);
+                ConstantUtf8* descriptorUTF8 = classFile.getConstant<ConstantUtf8>(nameAndType->descriptorIndex);
+                std::string methodName = methodNameUTF8->value;
+                std::string descriptor = descriptorUTF8->value;
+
+                std::vector<char> paramTypes = parseParameterTypes(descriptor);
+
+                std::vector<Value> args(paramTypes.size());
+                for (U4 i = paramTypes.size(); i > 0; )
+                {
+                    --i;
+                    args[i] = frame.pop();
+                }
+
+                Value objRefVal = frame.pop();
+                HeapObject** objectRef = std::get_if<HeapObject*>(&objRefVal);
+                if (!objectRef || !*objectRef)
+                {
+                    throw std::runtime_error("NullPointerException: invokeinterface on null reference");
+                }
+                if ((*objectRef)->type != HeapType::Object)
+                {
+                    throw std::runtime_error("invokeinterface: reference is not an object instance");
+                }
+
+                ObjectHeapObject* instance = static_cast<ObjectHeapObject*>(*objectRef);
+                ClassFile* targetClass = instance->javaClass;
+
+                const MethodInfo* targetMethod = targetClass->findMethod(methodName, descriptor);
+                if (!targetMethod)
+                {
+                    throw std::runtime_error("invokeinterface: method \"" + methodName + " " + descriptor + "\" not found");
+                }
+
+                const CodeAttribute* targetCode = targetClass->getCode(*targetMethod);
+                if (!targetCode)
+                {
+                    throw std::runtime_error("invokeinterface: method \"" + methodName + "\" has no Code attribute");
+                }
+
+                Frame invokedFrame(targetCode->maxLocals, targetCode->maxStack);
+                invokedFrame.setLocal(0, *objectRef);
+
+                U2 localSlot = 1;
+                for (U4 l = 0; l < paramTypes.size(); ++l)
+                {
+                    invokedFrame.locals[localSlot] = args[l];
+                    localSlot += (paramTypes[l] == 'J' || paramTypes[l] == 'D') ? 2 : 1;
+                }
+
+                FrameGuard invokedGuard(*this, invokedFrame);
+                Value result = execute(*targetClass, *targetCode);
+
+                if (descriptor.back() != 'V')
+                {
+                    frame.push(result);
+                }
+
+                break;
+            }
 
 
 
