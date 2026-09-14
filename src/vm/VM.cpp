f1dbde858d264e744e9c594c1f1eaaf71293d6df
VuqarAhadli <vuqarahadli17@gmail.com>
2026-09-15 00:21:23 +0200

    Ifnull opcode

--- src/vm/VM.cpp
+++ src/vm/VM.cpp
@@ -3743,6 +3743,28 @@ Value VM::execute(ClassFile& classFile, const CodeAttribute& code)
                     break;
                 }
 
+                case Opcode::IfNull:
+                {
+                    U4 opcodeStart = frame.programCounter - 1;
+
+                    U1 branchByte1 = bytecode[frame.programCounter];
+                    frame.programCounter++;
+                    U1 branchByte2 = bytecode[frame.programCounter];
+                    frame.programCounter++;
+
+                    S2 branchOffset = static_cast<S2>((branchByte1 << 8) | branchByte2);
+
+                    Value value = frame.pop();
+
+                    
+
+                    if (std::holds_alternative<HeapObject*>(value) && std::get<HeapObject*>(value) == nullptr)
+                    {
+                        frame.programCounter = opcodeStart + branchOffset;
+                    }
+
+                    break;
+                }
 
 
                 default:
